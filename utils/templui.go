6dde745c24dd793f2feba7dabc4fcf305c895a19
nhymxu <contact@dungnt.net>
2026-05-12 16:19:37 +0700

    chore: fix lint

--- utils/templui.go
+++ utils/templui.go
@@ -8,9 +8,8 @@ import (
 	"io"
 	"time"
 
-	"github.com/a-h/templ"
-
 	twmerge "github.com/Oudwins/tailwind-merge-go"
+	"github.com/a-h/templ"
 )
 
 // TwMerge combines Tailwind classes and resolves conflicts.
@@ -21,9 +20,11 @@ func TwMerge(classes ...string) string {
 // If returns value if condition is true, otherwise the zero value of T.
 func If[T any](condition bool, value T) T {
 	var empty T
+
 	if condition {
 		return value
 	}
+
 	return empty
 }
 
@@ -32,17 +33,20 @@ func IfElse[T any](condition bool, trueValue T, falseValue T) T {
 	if condition {
 		return trueValue
 	}
+
 	return falseValue
 }
 
 // MergeAttributes combines multiple Attributes into one.
 func MergeAttributes(attrs ...templ.Attributes) templ.Attributes {
 	merged := templ.Attributes{}
+
 	for _, attr := range attrs {
 		for k, v := range attr {
 			merged[k] = v
 		}
 	}
+
 	return merged
 }
 
@@ -69,32 +73,40 @@ var UseUnminifiedScripts = false
 func ComponentScript(component string) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
 		nonce := templ.GetNonce(ctx)
+
 		fileName := component + ".min.js"
 		if UseUnminifiedScripts {
 			fileName = component + ".js"
 		}
+
 		src := ScriptURL(componentScriptBasePath + "/" + fileName)
 
 		if _, err := io.WriteString(w, `<script type="module"`); err != nil {
 			return err
 		}
+
 		if nonce != "" {
 			if _, err := io.WriteString(w, ` nonce="`); err != nil {
 				return err
 			}
+
 			if _, err := io.WriteString(w, templ.EscapeString(nonce)); err != nil {
 				return err
 			}
+
 			if _, err := io.WriteString(w, `"`); err != nil {
 				return err
 			}
 		}
+
 		if _, err := io.WriteString(w, ` src="`); err != nil {
 			return err
 		}
+
 		if _, err := io.WriteString(w, templ.EscapeString(src)); err != nil {
 			return err
 		}
+
 		if _, err := io.WriteString(w, `"></script>`); err != nil {
 			return err
 		}
